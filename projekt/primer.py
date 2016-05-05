adfc3a712a7b3f8f02c4b044234a0b6d37f7338b
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2016-05-05 11:51:13 +0200

    Odstranjeni nepotrebni fetchall()

--- projekt/primer.py
+++ projekt/primer.py
@@ -9,7 +9,7 @@ import auth_public as auth
 
 # uvozimo psycopg2
 import psycopg2, psycopg2.extensions, psycopg2.extras
-psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s sumniki
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s šumniki
 
 # odkomentiraj, če želiš sporočila o napakah
 # debug(True)
@@ -17,12 +17,12 @@ psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo prob
 @get('/')
 def index():
     cur.execute("SELECT * FROM oseba ORDER BY priimek, ime")
-    return template('komitenti.html', osebe=cur.fetchall())
+    return template('komitenti.html', osebe=cur)
 
 @get('/transakcije/:x/')
 def transakcije(x):
     cur.execute("SELECT * FROM transakcija WHERE znesek > %s ORDER BY znesek, id", [int(x)])
-    return template('transakcije.html', x=x, transakcije=cur.fetchall())
+    return template('transakcije.html', x=x, transakcije=cur)
 
 
 
@@ -31,7 +31,7 @@ def transakcije(x):
 
 # priklopimo se na bazo
 conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password)
-conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) # onemogocimo transakcije
+conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) # onemogočimo transakcije
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
 
 # poženemo strežnik na portu 8080, glej http://localhost:8080/
