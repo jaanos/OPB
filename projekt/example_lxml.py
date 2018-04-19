f12f2bd13d528533bdef14520ab669e5d8ce5c03
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2018-04-19 11:36:01 +0200

    Dodan primer uvoza v bazo v Pythonu

--- projekt/example_lxml.py
+++ projekt/example_lxml.py
@@ -4,20 +4,27 @@
 from lxml import html
 import requests
 import csv
+import re
+
+def text(tag):
+    parts = [tag.text] + [text(t) for t in tag] + [tag.tail]
+    if tag.tag == 'br':
+        parts.insert(0, ' ')
+    return re.sub(r'\s+', ' ', ''.join(filter(None, parts)))
 
 # Naslov, od koder pobiramo podatke
 link = "https://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
 stran = html.fromstring(requests.get(link).content)
 
 # Preberemo prvo ustrezno tabelo
-tabela = [[c.xpath(".//a")[0].text if i == 0 and c.text is None else c.text
-          for i, c in enumerate(r.xpath("(th|td)"))] for r in
-          stran.xpath("//table[@class='wikitable sortable']")[0].xpath("tr")]
+tabela = [[text(c).replace('*', '').strip() for c in r.xpath("(th|td)")]
+          for r in  stran.xpath("//table[@class='wikitable sortable']")[0]
+                         .xpath("tr")]
 
 # Nadomestimo decimalne vejice in ločila tisočic ter pretvorimo v števila
-for i in range(1, len(tabela)):
-    tabela[i][1] = float(tabela[i][1].replace(",", "."))
-    tabela[i][2] = int(tabela[i][2].replace(".", ""))
+for r in tabela[1:]:
+    r[1] = float(r[1].replace(",", "."))
+    r[2] = int(r[2].replace(".", ""))
 
 # Zapišemo v datoteko CSV
 with open("obcine.csv", "w") as f:
