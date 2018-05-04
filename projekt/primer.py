aaec548fc7cf33f26964db40535b5e898cce0322
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2018-05-04 13:19:26 +0200

    Dodan obrazec, CSS in osnova

--- projekt/primer.py
+++ projekt/primer.py
@@ -14,6 +14,10 @@ psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo prob
 # odkomentiraj, če želiš sporočila o napakah
 # debug(True)
 
+@get('/static/<filename:path>')
+def static(filename):
+    return static_file(filename, root='static')
+
 @get('/')
 def index():
     cur.execute("SELECT * FROM oseba ORDER BY priimek, ime")
@@ -24,7 +28,22 @@ def transakcije(x):
     cur.execute("SELECT * FROM transakcija WHERE znesek > %s ORDER BY znesek, id", [int(x)])
     return template('transakcije.html', x=x, transakcije=cur)
 
+@get('/dodaj_transakcijo')
+def dodaj_transakcijo():
+    return template('dodaj_transakcijo.html', znesek='', racun='', opis='', napaka=None)
 
+@post('/dodaj_transakcijo')
+def dodaj_transakcijo_post():
+    znesek = request.forms.znesek
+    racun = request.forms.racun
+    opis = request.forms.opis
+    try:
+        cur.execute("INSERT INTO transakcija (znesek, racun, opis) VALUES (%s, %s, %s)",
+                    (znesek, racun, opis))
+    except Exception as ex:
+        return template('dodaj_transakcijo.html', znesek=znesek, racun=racun, opis=opis,
+                        napaka = 'Zgodila se je napaka: %s' % ex)
+    redirect("/")
 
 ######################################################################
 # Glavni program
@@ -35,4 +54,4 @@ conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) # onemo
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
 
 # poženemo strežnik na portu 8080, glej http://localhost:8080/
-run(host='localhost', port=8080)
+run(host='localhost', port=8080, reloader=True)
