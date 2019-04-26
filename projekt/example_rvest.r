40dcbcc082bf6df0f97b7fe59f67f438084a69cb
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2019-04-26 11:19:46 +0200

    Uporabljena knjižnica readr v primeru za rvest

--- projekt/example_rvest.r
+++ projekt/example_rvest.r
@@ -2,6 +2,7 @@
 library(rvest)
 library(dplyr)
 library(gsubfn)
+library(readr)
 
 # Naslov, od koder pobiramo podatke
 link <- "https://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
@@ -12,10 +13,11 @@ tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
   .[[1]] %>% html_table()
 
 # Nadomestimo decimalne vejice in ločila tisočic ter pretvorimo v števila
-tabela[[2]] <- tabela[[2]] %>% strapplyc("([0-9]+)") %>%
-  sapply(paste, collapse = ".") %>% as.numeric()
-tabela[[3]] <- tabela[[3]] %>% strapplyc("([0-9]+)") %>%
-  sapply(paste, collapse = "") %>% as.numeric()
+sl <- locale(decimal_mark=",", grouping_mark=".")
+for (i in c(2, 3, 5, 6)) {
+  tabela[[i]] <- tabela[[i]] %>% parse_number(na="-", locale=sl)
+}
+tabela[[9]] <- tabela[[9]] %>% parse_character(na="-")
 
 # Zapišemo v datoteko CSV
-write.csv(tabela, "obcine.csv")
+write_csv(tabela, "obcine.csv", na="")
