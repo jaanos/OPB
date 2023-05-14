bdc9ca5138fad4f914ac3933738e092f2a1afb41
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2023-05-14 20:28:30 +0200

    Dodani manjkajoči datoteki od prejšnjega tedna

---
+++ predavanja/primeri/banka/static/banka.js
@@ -0,0 +1,16 @@
+'use strict'
+
+let filtriraj = function(niz, stolpci=[1]) {
+    let podatki = document.getElementById("podatki");
+    [...podatki.rows].forEach(vrstica => {
+        if (vrstica.cells[0].tagName.toLocaleLowerCase() == "th") {
+            return;
+        }
+        if (stolpci.some(stolpec =>
+                vrstica.cells[stolpec].innerText.toLocaleLowerCase().indexOf(niz) >= 0)) {
+            vrstica.style.display = '';
+        } else {
+            vrstica.style.display = 'none';
+        }
+    })
+}
