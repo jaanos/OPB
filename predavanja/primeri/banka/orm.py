a7d6eeaf2d88e7cd09e7c90a263865bb7cb08683
Janoš Vidali <janos.vidali@fmf.uni-lj.si>
2024-03-26 17:41:21 +0100

    Posplošeno pridobivanje entitet iz baze

--- predavanja/primeri/banka/orm.py
+++ predavanja/primeri/banka/orm.py
@@ -250,6 +250,50 @@ class Entiteta:
                                             vrednost=sql.Literal(vrednost + 1)))
 
     @classmethod
+    def _parametri(cls, **kwargs):
+        """
+        Vrni parametre za pridobivanje podatkov iz baze.
+        """
+        parametri = cls.__tabela_kljuc()
+        parametri.update(
+            stolpci=sql.SQL(", ").join(
+                sql.Identifier(stolpec) for stolpec in cls.STOLPCI
+            ),
+            urejanje=parametri['kljuc'],
+            zdruzevanje=None
+        )
+        parametri.update(kwargs)
+        for polje, beseda in (
+                    ('stolpci', ""),
+                    ('urejanje', "ORDER BY"),
+                    ('zdruzevanje', "GROUP BY")
+                ):
+            if parametri[polje] is None:
+                parametri[polje] = sql.SQL("")
+            else:
+                stolpci = parametri[polje]
+                if not isinstance(stolpci, (list, tuple)):
+                    stolpci = [stolpci]
+                parametri[polje] = sql.SQL(
+                    "{beseda} {stolpci}"
+                ).format(
+                    beseda=sql.SQL(beseda),
+                    stolpci=sql.SQL(", ").join(
+                        stolpec if isinstance(stolpec, sql.Composable)
+                        else sql.Identifier(stolpec)
+                        for stolpec in stolpci
+                    )
+                )
+        return parametri
+
+    @classmethod
+    def _iz_baze(cls, *vrstica):
+        """
+        Ustvari entiteto s podatki iz baze.
+        """
+        return cls(*vrstica)
+
+    @classmethod
     def z_id(cls, id):
         """
         Vrni entiteto s podanim ID-jem.
@@ -257,33 +301,46 @@ class Entiteta:
         with conn.cursor() as cur:
             cur.execute(sql.SQL("""
                     SELECT {stolpci} FROM {tabela}
-                    WHERE {kljuc} = %s;
-                """).format(stolpci=sql.SQL(", ").join(
-                                sql.Identifier(stolpec)
-                                for stolpec in cls.STOLPCI
-                            ),
-                            **cls.__tabela_kljuc()), [id])
+                     WHERE {kljuc} = %s
+                    {zdruzevanje};
+                """).format(**cls._parametri()), [id])
             vrstica = cur.fetchone()
             if not vrstica:
                 raise ValueError(f'{cls.__name__} z ID-jem {id} ne obstaja!')
-            return cls(*vrstica)
+            return cls._iz_baze(*vrstica)
 
     @classmethod
-    def seznam(cls):
+    def seznam(cls, urejanje=None, /, **kwargs):
         """
         Vračaj entitete iz baze.
         """
         with conn.cursor() as cur:
             cur.execute(sql.SQL("""
                     SELECT {stolpci} FROM {tabela}
-                    ORDER BY {kljuc};
-                """).format(stolpci=sql.SQL(", ").join(
-                                sql.Identifier(stolpec)
-                                for stolpec in cls.STOLPCI
-                            ),
-                            **cls.__tabela_kljuc()))
+                    {filtriranje}
+                    {zdruzevanje}
+                    {urejanje};
+                """).format(**cls._parametri(urejanje=urejanje),
+                            filtriranje=sql.SQL("WHERE {pogoji}").format(
+                                pogoji=sql.SQL(" AND ").join(
+                                    sql.SQL(
+                                        "{stolpec} = {vrednost}"
+                                    ).format(
+                                        stolpec=sql.Identifier(stolpec),
+                                        vrednost=sql.Placeholder(stolpec)
+                                    )
+                                    for stolpec in kwargs
+                                )
+                            )
+                            if kwargs else sql.SQL("")),
+                            {
+                                stolpec: vrednost.glavni_kljuc()
+                                    if isinstance(vrednost, Entiteta)
+                                    else vrednost
+                                for stolpec, vrednost in kwargs.items()
+                            })
             for vrstica in cur:
-                yield cls(*vrstica)
+                yield cls._iz_baze(*vrstica)
 
     def glavni_kljuc(self):
         """
@@ -324,7 +381,7 @@ class Entiteta:
                     generirani = {ime for ime, stolpec in self.STOLPCI.items()
                                     if (stolpec.privzeto or
                                         stolpec.tip == "SERIAL")
-                                    and self[ime] is None}
+                                    and vrednosti[ime] is None}
                     vrednosti = {ime: vrednost for ime, vrednost
                                     in vrednosti.items()
                                     if ime not in generirani}
